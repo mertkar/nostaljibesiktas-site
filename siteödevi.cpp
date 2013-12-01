1319d220fc98ad57a790aed666a6ad0c79ef4ea5
mertkar <mert_karabulut_bjk@hotmail.com>
2013-12-01 21:54:12 +0200

    siteödevi.cpp

---
+++ siteödevi.cpp
@@ -0,0 +1,267 @@
+
+<html> 
+<head> <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Beşiktaş'ın Avrupa Tarihi </title>
+
+<link REL="SHORTCUT ICON" HREF="https://www.bjk.com.tr/assets/favicon.ico">
+
+
+</head> 
+
+<center>
+<body style="background-color : #C0B5B5" background="b12.jpg">
+</center>
+
+<p align="right"> <b><font face="Times New Roman" size="3" color="#000000"> Beşiktaş'ı takip etmek için</font><br></p></b>
+<div class="social_buttons">
+<p align="right">
+							<a href="http://facebook.com/besiktas" target="_blank"><img src="https://www.bjk.com.tr/img/facebook.png" /></a> 
+							<a href="http://twitter.com/besiktas" target="_blank"><img src="https://www.bjk.com.tr/img/twitter.png" /></a>
+							<a href="https://plus.google.com/+Besiktas" target="_blank" rel="publisher"><img src="https://www.bjk.com.tr/img/google-plus-icon.png" /></a>
+							<a href="http://youtube.com/Besiktas" target="_blank"><img src="https://www.bjk.com.tr/img/youtube-icon.png" /></a>
+							</div></p>
+							
+							
+&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
+<a href="http://www.uefa.com/uefachampionsleague/" target="_blank"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Logo_uefa_2012.png/120px-Logo_uefa_2012.png" /></a>
+<a href="https://www.bjk.com.tr/tr/"target="_blank"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Logo_of_Be%C5%9Fikta%C5%9F_JK.svg/100px-Logo_of_Be%C5%9Fikta%C5%9F_JK.svg.png" alt="some_text"></a>
+<a href="http://www.uefa.com/uefaeuropaleague/"target="_blank"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Logo_uefa_europa_2012.png/110px-Logo_uefa_europa_2012.png" alt="some_text"></a>
+
+<h1 align="middle">Beşiktaş'ın Unutulmaz Avrupa Maçları</h1>
+<center>
+<p align="middle"><b><font face="Comic Sans" size="3" color="#000000"> Beşiktaş Jimnastik Kulübü 1957-58 sezonunda Yunanistan'ın Olympiakos CFP takımına karşı oynadığı maçla beraber Avrupa</p> 
+<p align="middle">serüvenine başlamıştır. Şampiyon Kulüpler Kupası'nda 1986-87 sezonunda çeyrek final gören Beşiktaş,</p>
+<p align="middle"> UEFA Kupası'nda en iyi derecesini 2002-03 sezonunda, Mircea Lucescu yönetiminde çeyrek finale çıkarak yapmış, bu turda</p> 
+<p align="middle">İtalya'dan Lazio'ya elenmiştir. 2003-04 sezonunda ise, Şampiyonlar Ligi'nde yine Lazio'nun grubuna düşen Beşiktaş,</p>
+<p align="middle"> son maçta Sparta Prag'ın Lazio'ya attığı golle grubunda 3. oldu ve yoluna UEFA Kupası'nda devam etti.</p>
+<center>	
+
+ <h3><font size="4" color="#DE030E"> 2002-2003 Uefa Kupası </h3></font>
+
+ <li> 1.Tur  </li> <br>19.09.2002 Beşiktaş 2-0 <a href="http://tr.wikipedia.org/wiki/FK_Sarajevo" target="_blank" title="FK Sarajevo">Sarajevo</a> Goller : Pancu(30), Ahmet D.(32)<br>
+ 03.10.2002 <a href="http://tr.wikipedia.org/wiki/FK_Sarajevo" target="_blank" title="FK Sarajevo">Sarajevo</a> 0-5 Beşiktaş     Goller : Pancu(5), İbrahim Ü.(42), Ahmet D.(70), Yasin S.(83), Ali C.(84)</br> 
+ <img src="sara.jpg" width="300" height="200"><br>
+ <br><li> 2.Tur </li> <br>31.10.2002 <a href="http://tr.wikipedia.org/wiki/Deportivo_Alav%C3%A9s" target="_blank" title="Alaves">Alaves</a> 1-1 Beşiktaş Goller : Karmona(31)<br>
+ 14.11.2002 Beşiktaş 1-0 <a href="http://tr.wikipedia.org/wiki/Deportivo_Alav%C3%A9s" target="_blank" title="Alaves">Alaves</a> Goller : İlhan Mansız(7)</br>
+ <img src="alav.jpg" width="300" height="200"><br>
+ <br><li>3.Tur </li> <br>28.11.2002 Beşiktaş 3-1 <a href="http://tr.wikipedia.org/wiki/FK_Dinamo_Kiev" target="_blank" title="Dinamo Kiev">Dinamo Kiev</a> Goller : Pancu(30), Ronaldo(71), Nouma(82)<br>
+ 12.12.2002 <a href="http://tr.wikipedia.org/wiki/FK_Dinamo_Kiev" target="_blank" title="Dinamo Kiev">Dinamo Kiev</a> 0-0 Beşiktaş</br>
+ <img src="kiev.jpg" width="300" height="200"><br>
+ <br><li> 4.Tur  </li> <br> 20.02.2003 <a href="http://tr.wikipedia.org/wiki/SK_Slavia_Prag" target="_blank" title="Slavia Prag">Slavia Prag</a> 1-0 Beşiktaş <br>
+ 27.02.2003 Beşiktaş 4-2 <a href="http://tr.wikipedia.org/wiki/SK_Slavia_Prag" target="_blank" title="Slavia Prag">Slavia Prag</a>     Goller : Pancu(41),Ronaldo(61), Ahmet D.(66), İlhan M.(70)</br>
+ <img src="slavia.jpg" width="300" height="200"><br>
+ <br><li> Çeyrel Final </li> <br> 13.03.2003 <a href="http://tr.wikipedia.org/wiki/SS_Lazio" target="_blank" title="Lazio">Lazio</a> 1-0 Beşiktaş<br>
+ 20.03.2003 Beşiktaş 1-2 <a href="http://tr.wikipedia.org/wiki/SS_Lazio" target="_blank" title="Lazio">Lazio</a> Goller : Sergen Yalçın(82) </br>
+<img src="lazio.jpg" width="300" height="200"><br>
+
+<center>
+<br><p align="middle"> Beşiktaş şimdiye kadar toplam üç farklı UEFA turnuvasına katılmıştır.</p> 
+<p align="middle"> Bu listede bu turnuvalarda oynadığı sezon, oynadığı maç, galibiyet, beraberlik ve mağlubiyet sayıları gösterilmektedir.</p></font></br>
+</center>
+											
+<table align="middle">
+<tr>
+<th>Turnuva</th>
+<th scope="col"><span title="Sezon">S</span></th>
+<th scope="col"><span title="Oynadığı maç">O</span></th>
+<th scope="col"><span title="Galibiyet">G</span></th>
+<th scope="col"><span title="Beraberlik">B</span></th>
+<th scope="col"><span title="Mağlubiyet">M</span></th>
+<th scope="col"><span title="Atılan gol">AG</span></th>
+<th scope="col"><span title="Yenilen gol">YN</span></th>
+</tr>
+<tr>
+<th scope="row"><a href="http://www.uefa.com/uefachampionsleague/" target="_blank" title="UEFA Şampiyonlar Ligi">Şampiyonlar Ligi / Şampiyon Kulüpler Kupası</a></th>
+<td align="center">16</td>
+<td align="center">64</td>
+<td align="center">20</td>
+<td align="center">12</td>
+<td align="center">32</td>
+<td align="center">59</td>
+<td align="center">101</td>
+</tr>
+<tr>
+<th scope="row"><a href="http://www.uefa.com/uefaeuropaleague/" target="_blank" title="UEFA Avrupa Ligi">Avrupa Ligi / UEFA Kupası</a></th>
+<td align="center">13</td>
+<td align="center">79</td>
+<td align="center">36</td>
+<td align="center">14</td>
+<td align="center">30</td>
+<td align="center">125</td>
+<td align="center">100</td>
+</tr>
+<tr>
+<th scope="row"><a href="http://www.uefa.com/uefachampionsleague/" target="_blank" title="UEFA Kupa Galipleri Kupası">Kupa Galipleri Kupası</a></th>
+<td align="center">7</td>
+<td align="center">20</td>
+<td align="center">4</td>
+<td align="center">4</td>
+<td align="center">12</td>
+<td align="center">21</td>
+<td align="center">38</td>
+</tr>
+<tr class="sortbottom">
+<th scope="row">Toplam</th>
+<td align="center">36</td>
+<td align="center">163</td>
+<td align="center">60</td>
+<td align="center">30</td>
+<td align="center">74</td>
+<td align="center">205</td>
+<td align="center">237</td>
+</tr>
+</table></center>
+
+<h2 align="middle">2013-2014 Sezonu Beşiktaş Videosu</h2>
+
+<div style="text-align:center"> 
+  <button onclick="playPause()">Play/Pause</button> 
+  <button onclick="makeBig()">Big</button>
+  <button onclick="makeSmall()">Small</button>
+  <button onclick="makeNormal()">Normal</button>
+  <br> 
+  <video id="video1" width="420">
+    <source src="bjjk.mp4" type="video/mp4">
+    <source src="mov_bbb.ogg" type="video/ogg">
+    Your browser does not support HTML5 video.
+  </video>
+</div> 
+
+<script> 
+var myVideo=document.getElementById("video1"); 
+
+function playPause()
+{ 
+if (myVideo.paused) 
+  myVideo.play(); 
+else 
+  myVideo.pause(); 
+} 
+
+function makeBig()
+{ 
+myVideo.width=560; 
+} 
+
+function makeSmall()
+{ 
+myVideo.width=320; 
+} 
+
+function makeNormal()
+{ 
+myVideo.width=420; 
+} 
+</script><br>
+
+<table>
+<tr><h2 align="middle"> Şimdi unutulmaz Beşiktaş maçlarına göz atalım.</h2>
+
+<td><a href="http://www.youtube.com/watch?v=Y6XfP7iMcdw" target="_blank">
+<p align="middle"> <b><font face="Times New Roman" size="3" color="#DE030E"> İnönü'de tarihi galibiyet. Beşiktaş 3-0 Barcelona 19.09.2000</font><br ></p></b></a>
+<img src="bar.jpg" width="320" height="250"></td>
+<td><a href="http://www.youtube.com/watch?v=IsNWKnq2tl8"target="_blank">
+<p align="middle"> <b><font face="Times New Roman" size="3" color="#DE030E"> Stanford Bridge'de unutulmaz zafer. Chelsea 0-2 Beşiktaş 01.10.2003</font><br ></p></b></a>
+<img src="chel.jpg" width="320" height="250"></td>
+
+<td><a href="http://www.youtube.com/watch?v=b8BJyIj-WbM"target="_blank">
+<p align="middle"> <b><font face="Times New Roman" size="3" color="#DE030E"> 12.futbolcu Beşiktaş tribünleriydi. Beşiktaş 2-1 Liverpool 25.10.2007</font><br ></p></b></a>
+<img src="liver.jpg" width="320" height="250"></td>
+
+<td><a href="http://www.youtube.com/watch?v=Iz5uf29NQ64"target="_blank">
+<p align="middle"> <b><font face="Times New Roman" size="3" color="#DE030E"> Old Trafford'da skor tabelası böyleydi. Manchester United 0-1 Beşiktaş 16.09.2009</font><br ></p></b></a>
+<img src="manu.jpg" width="320" height="250"></td>
+<tr></table><br>
+
+
+<table>
+<tr>
+<h1 align="middle"> Beşiktaş Tezahüratları </h1><br>
+
+<td><b><font face="Times New Roman" size="3" color="#DE030E"><p align="middle"> Sevemez Kimse Seni Benim Sevdiğim Kadar </p></font></b>
+
+<audio controls>
+  <source src="horse.ogg" type="audio/ogg">
+  <source src="sevemez.mp3" type="audio/mpeg">
+Your browser does not support the audio element.
+</audio></td>
+
+<td><b><font face="Times New Roman" size="3" color="#DE030E"><p align="middle"> Gücüne Güç Katmaya Geldik </p></font></b>
+
+<audio controls>
+  <source src="horse.ogg" type="audio/ogg">
+  <source src="guc.mp3" type="audio/mpeg">
+Your browser does not support the audio element.
+</audio></td>
+
+<td><b><font face="Times New Roman" size="3" color="#DE030E"><p align="middle"> Bir Derdim Var Bin Dermana Değişmem Asla </p></font></b>
+<audio controls>
+  <source src="horse.ogg" type="audio/ogg">
+  <source src="degismem.mp3" type="audio/mpeg">
+Your browser does not support the audio element.
+</audio></td>
+
+<td><b><font face="Times New Roman" size="3" color="#DE030E"><p align="middle"> Yağmurlu Bir Günde Görmüştüm Seni </p></font></p>
+
+<audio controls>
+  <source src="horse.ogg" type="audio/ogg">
+  <source src="yagmur.mp3" type="audio/mpeg">
+Your browser does not support the audio element.
+</audio></td>
+<tr></table>
+
+
+<h3 align= "middle"> Site hakkındaki eleştirileriniz için :</h3>
+
+<form action="MAILTO:mert_karabulut_bjk@hotmail.com" method="post" enctype="text/plain">
+<p align= "middle"> Adınız Soyadınız:<br>
+<input type="text" name="" value=""><br>
+Mail Adresiniz:<br>
+<input type="text" name="" value=""><br>
+Yorumunuz:<br>
+<input type="text" name="" value="" size="50"><br><br>
+<input type="submit" value="Gönder">
+<input type="reset" value="Reset"></p>
+
+
+<div class="elapsed_time"><!-- 0.3861 --></div>
+										<div class='footer_links'><!-- footer_links start -->
+						
+						<p align="middle"><b><font face="Times New Roman" size="3" color="#000000"> Beşiktaş Sponsorları</font><br ></p></b>
+					</div><!-- footer_links end -->
+					<!-- SPONSORS -->
+					
+<center>												
+<table>
+<tr>
+<th scope="col"><span style="cursor:help; border-bottom:1px dotted;" title="Sezon"></span></th>
+<th scope="col"><span style="cursor:help; border-bottom:1px dotted;" title="Oynadığı maç"></span></th>
+<th scope="col"><span style="cursor:help; border-bottom:1px dotted;" title="Galibiyet"></span></th>
+<th scope="col"><span style="cursor:help; border-bottom:1px dotted;" title="Beraberlik"></span></th>
+
+					<div class="sponsors1">
+							<td><div class='sponsor_item1'><a target="_blank" href="http://www.vodafone.com.tr"><img src="https://www.bjk.com.tr/img/sponsors/vodafone_logo_small.png"></a></div></td> 
+							<td><div class='sponsor_item1'><a target="_blank" href="http://www.adidas.com.tr"><img src="https://www.bjk.com.tr/img/sponsors/adidas.png"></a></div></td>
+							<td><div class='sponsor_item1'><a target="_blank" href="http://www.toyota.com.tr"><img src="https://www.bjk.com.tr/img/sponsors/toyota.png"></a></div></td>
+							<td><div class='sponsor_item1'><a target="_blank" href="http://www.acibadem.com.tr"><img src="https://www.bjk.com.tr/img/sponsors/acibadem_logo.png"></a></div></td>
+							</div>
+				
+					<div class='sponsors2'>
+							
+							<td><div class='sponsor_item2'><a target="_blank" href="http://www.turkishairlines.com"><img src="https://www.bjk.com.tr/img/sponsors/thy.png"></a></div>	</td>
+							<td><div class='sponsor_item2'><a target="_blank" href="http://www.sarar.com.tr"><img src="https://www.bjk.com.tr/img/sponsors/sarar.png"></a></div></td>
+							<td><div class='sponsor_item2'><a target="_blank" href="http://www.dogusotomotiv.com.tr"><img src="https://www.bjk.com.tr/img/sponsors/dogus_otomotiv.png"></a></div></td>
+							
+					</div>
+			</div>
+		</div>
+	</div>
+</table>
+<div id="footer" style="background-color:#000000;clear:both;text-align:center;">
+<a href="http://twitter.com/merttkrblt"target="_blank">
+<p><font color= "#FFFFFE"> Copyright © twitter.com/merttkrblt </Font></p></div></a>
+
+</body> 
+
+
+</html> 
